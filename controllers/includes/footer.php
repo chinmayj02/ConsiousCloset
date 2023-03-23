9d497f78b9e0faf1a8fd3d30ea73e83416c7b43a
Chinmay Joshi <chinmayujoshi@gmail.com>
2023-03-23 17:22:10 +0530

    homepage

--- controllers/includes/footer.php
+++ controllers/includes/footer.php
@@ -1,108 +1,48 @@
 <footer class="full-footer" data-v-186a190d data-v-b4e1f63a><svg width="1440" height="88" viewBox="0 0 1440 88" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" class="full-footer__wave" data-v-186a190d data-v-186a190d>
         <path d="M0 47.577c95.583 0 106.97 8.729 226.979 8.729 120.009 0 176.224-34.103 296.233-34.103 105.102 0 97.379 13.486 217.388 13.486C833.777 35.689 900.66 0 1008.17 0c120.01 0 116.52 20.533 236.5 20.533 119.97 0 114.69-11.924 195.33-11.924V88H0V47.577z" fill="currentColor" data-v-186a190d data-v-186a190d></path>
     </svg>
-    <div class="full-footer__content" data-v-186a190d><a href="https://www.bcorporation.net/find-a-b-corp/company/ecosia-gmbh/" target="_blank" rel="noopener" data-track-id="footer" class="footer-bcorp" data-v-186a190d><span class="sr-only">
-                B-Corporation Certificate
-            </span></a>
-        <div class="footer-menu" data-v-186a190d>
-            <p data-test-id="footer-menu-title" class="footer-menu__title">
-                What we do
-            </p>
-            <ul>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="whattitle" href="https://info.ecosia.org/what" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        How Ecosia works
-                    </a></li>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="whattitle" href="https://ecosia.co/finreportsen" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Financial reports
-                    </a></li>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="whattitle" href="https://ecosia.helpscoutdocs.com/article/108-how-to-advertise-on-ecosia" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Advertising on Ecosia
-                    </a></li>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="whattitle" href="https://plant.ecosia.org" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Gift trees
-                    </a></li>
-            </ul>
-        </div>
-        <div class="footer-menu" data-v-186a190d>
-            <p data-test-id="footer-menu-title" class="footer-menu__title">
-                About
-            </p>
-            <ul>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="abouttitle" href="https://explore.ecosia.org/jobs" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Careers
-                    </a></li>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="abouttitle" href="https://info.ecosia.org/press" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Press
-                    </a></li>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="abouttitle" href="https://blog.ecosia.org" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Blog
-                    </a></li>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="abouttitle" href="https://ecosia.helpscoutdocs.com/" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Help
-                    </a></li>
-            </ul>
-        </div>
-        <div class="footer-menu" data-v-186a190d>
-            <p data-test-id="footer-menu-title" class="footer-menu__title">
-                Legal
-            </p>
-            <ul>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="legaltitle" href="https://info.ecosia.org/privacy" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Privacy policy
-                    </a></li>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="legaltitle" href="https://explore.ecosia.org/ecosia-search-tos" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Terms of service
-                    </a></li>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="legaltitle" href="https://info.ecosia.org/imprint" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Imprint
-                    </a></li>
-                <li class="footer-menu__link"><a data-track-id="footer" data-test-id="legaltitle" href="https://ecosia.helpscoutdocs.com/article/403-wie-ecosia-ihre-inhalte-einordnet" rel="noopener" class="link link--as-a link--color-secondary" data-v-b4e44ddc>
-                        Transparency information MStV
-                    </a></li>
-            </ul>
-        </div>
+    <div class="full-footer__content" data-v-186a190d>
+        
+        
     </div>
     <div class="footer-social full-footer__social" data-v-186a190d>
         <ul class="footer-social__list">
-            <li class="footer-social__item"><a href="https://www.instagram.com/ecosia" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-instagram icon--size-m" data-v-16fce65c data-v-16fce65c>
+            <li class="footer-social__item"><a href="" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-instagram icon--size-m" data-v-16fce65c data-v-16fce65c>
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12zm11-5.867c-1.593 0-1.793.007-2.418.036-.625.028-1.051.127-1.424.272-.386.15-.714.35-1.04.677a2.88 2.88 0 00-.677 1.039c-.145.373-.244.8-.272 1.424-.028.626-.036.826-.036 2.42 0 1.592.007 1.792.036 2.418.029.624.127 1.05.272 1.423.15.386.35.714.677 1.04.326.326.653.527 1.039.677.373.145.8.244 1.424.272.626.029.826.036 2.419.036s1.793-.007 2.418-.036c.625-.028 1.052-.127 1.425-.272.386-.15.713-.351 1.038-.677a2.88 2.88 0 00.678-1.04c.144-.373.243-.8.272-1.423.028-.626.036-.826.036-2.419s-.008-1.793-.036-2.419c-.03-.625-.128-1.05-.272-1.424a2.88 2.88 0 00-.678-1.04 2.866 2.866 0 00-1.038-.676c-.374-.145-.8-.244-1.425-.272-.626-.029-.825-.036-2.42-.036h.003zm-.526 1.058h.527c1.566 0 1.752.005 2.37.033.572.026.883.122 1.09.202.273.107.468.234.674.44.205.204.332.4.439.674.08.206.176.517.202 1.089.028.618.034.804.034 2.37 0 1.565-.006 1.751-.034 2.37-.026.572-.122.882-.202 1.089a1.814 1.814 0 01-.44.674c-.205.205-.4.332-.674.438-.206.081-.517.176-1.089.203-.618.028-.804.034-2.37.034-1.567 0-1.753-.006-2.371-.034-.572-.027-.883-.122-1.09-.203a1.817 1.817 0 01-.674-.439 1.819 1.819 0 01-.44-.674c-.08-.206-.175-.517-.201-1.089-.028-.618-.034-.804-.034-2.37 0-1.567.006-1.752.034-2.37.026-.573.122-.883.202-1.09.106-.274.234-.47.439-.675.205-.205.4-.332.675-.439.206-.08.517-.176 1.089-.202.541-.024.75-.032 1.844-.033v.002zm3.658.974a.704.704 0 100 1.408.704.704 0 000-1.409zm-3.131.822a3.013 3.013 0 100 6.026 3.013 3.013 0 000-6.026zm0 1.057a1.956 1.956 0 110 3.912 1.956 1.956 0 010-3.912z" fill="currentColor" data-v-16fce65c data-v-16fce65c></path>
                     </svg> <span class="sr-only" data-v-b4e44ddc>
                         instagram
                     </span></a></li>
-            <li class="footer-social__item"><a href="https://www.youtube.com/user/EcosiaORG" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-youtube icon--size-m" data-v-16fce65c data-v-16fce65c>
+            <li class="footer-social__item"><a href="" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-youtube icon--size-m" data-v-16fce65c data-v-16fce65c>
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M1 12.015C1 5.932 5.858 1 11.85 1 17.843 1 22.7 5.932 22.7 12.015c0 6.084-4.857 11.015-10.85 11.015C5.858 23.03 1 18.1 1 12.015zm16.497-2.217s-.113-.827-.459-1.192c-.44-.478-.932-.48-1.158-.508-1.617-.122-4.043-.122-4.043-.122h-.005s-2.426 0-4.043.122c-.226.028-.718.03-1.158.508-.347.365-.46 1.192-.46 1.192s-.115.972-.115 1.943v.911c0 .972.116 1.944.116 1.944s.112.827.459 1.191c.44.479 1.017.464 1.274.513.925.093 3.93.121 3.93.121s2.428-.003 4.045-.125c.226-.028.719-.03 1.158-.509.346-.364.46-1.192.46-1.192s.115-.971.115-1.943v-.91c0-.972-.116-1.944-.116-1.944zm-6.856 3.958v-3.373l3.123 1.692-3.123 1.681z" fill="currentColor" data-v-16fce65c data-v-16fce65c></path>
                     </svg> <span class="sr-only" data-v-b4e44ddc>
                         youtube
                     </span></a></li>
-            <li class="footer-social__item"><a href="https://twitter.com/ecosia" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-twitter icon--size-m" data-v-16fce65c data-v-16fce65c>
+            <li class="footer-social__item"><a href="" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-twitter icon--size-m" data-v-16fce65c data-v-16fce65c>
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12zm10.67-2.06l.024.382-.385-.047c-1.4-.179-2.624-.785-3.663-1.802l-.507-.505-.131.373c-.277.83-.1 1.709.477 2.299.308.326.238.373-.293.179-.184-.062-.346-.109-.361-.086-.054.055.13.761.277 1.041.2.389.608.77 1.054.994l.377.18-.446.007c-.431 0-.447.008-.4.17.154.506.761 1.042 1.438 1.275l.477.163-.415.248a4.332 4.332 0 01-2.062.575c-.346.008-.631.039-.631.062 0 .078.939.513 1.485.684 1.639.505 3.585.287 5.047-.575 1.04-.614 2.078-1.833 2.563-3.014.261-.629.523-1.779.523-2.33 0-.358.023-.404.454-.831.254-.249.492-.52.538-.598.077-.148.07-.148-.323-.016-.654.233-.746.202-.423-.148.239-.248.523-.699.523-.83 0-.024-.115.015-.246.085a4.295 4.295 0 01-.677.264l-.415.132-.378-.256c-.207-.14-.5-.296-.654-.342-.392-.109-.992-.093-1.346.03-.962.35-1.57 1.251-1.5 2.238z" fill="currentColor" data-v-16fce65c data-v-16fce65c></path>
                     </svg> <span class="sr-only" data-v-b4e44ddc>
                         twitter
                     </span></a></li>
-            <li class="footer-social__item"><a href="https://www.linkedin.com/company/ecosia" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-linkedin icon--size-m" data-v-16fce65c data-v-16fce65c>
+            <li class="footer-social__item"><a href="" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-linkedin icon--size-m" data-v-16fce65c data-v-16fce65c>
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M12 23C5.925 23 1 18.075 1 12S5.925 1 12 1s11 4.925 11 11-4.925 11-11 11zM7.715 9.161c.757 0 1.371-.56 1.371-1.251 0-.692-.614-1.253-1.371-1.253-.758 0-1.372.56-1.372 1.253 0 .691.614 1.251 1.372 1.251zm-1.058 8.05h2.46v-7.097h-2.46v7.096zm5.914-3.947c0-.814.385-1.61 1.305-1.61.92 0 1.147.796 1.147 1.59v3.875h2.45v-4.033c0-2.803-1.657-3.286-2.676-3.286-1.019 0-1.58.344-2.226 1.179v-.955h-2.457v7.095h2.457v-3.855z" fill="currentColor" data-v-16fce65c data-v-16fce65c></path>
                     </svg> <span class="sr-only" data-v-b4e44ddc>
                         linkedin
                     </span></a></li>
-            <li class="footer-social__item"><a href="https://www.tiktok.com/@ecosia" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-tiktok icon--size-m" data-v-16fce65c data-v-16fce65c>
+            <li class="footer-social__item"><a href="" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-tiktok icon--size-m" data-v-16fce65c data-v-16fce65c>
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M1 12C1 5.925 5.925 1 12 1a11 11 0 0111 11c0 6.075-4.925 11-11 11S1 18.075 1 12zm16.582-2.913v-.473a3.115 3.115 0 01-3.102-2.828V5.5h-2.24v8.887a1.882 1.882 0 11-1.31-1.8v-2.274a4.114 4.114 0 10-.565 8.187 4.114 4.114 0 004.115-4.113v-4.54a5.32 5.32 0 003.102.992V9.087z" fill="currentColor" data-v-16fce65c data-v-16fce65c></path>
                     </svg> <span class="sr-only" data-v-b4e44ddc>
                         tiktok
                     </span></a></li>
-            <li class="footer-social__item"><a href="https://www.facebook.com/ecosia" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-facebook icon--size-m" data-v-16fce65c data-v-16fce65c>
+            <li class="footer-social__item"><a href="" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-facebook icon--size-m" data-v-16fce65c data-v-16fce65c>
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12zm11.78 6.101v-5.984h1.652l.219-2.063H12.78l.003-1.032c0-.538.05-.826.823-.826h1.033V6.133h-1.652c-1.985 0-2.683 1-2.683 2.683v1.238H9.067v2.063h1.237V18.1h2.476z" fill="currentColor" data-v-16fce65c data-v-16fce65c></path>
                     </svg> <span class="sr-only" data-v-b4e44ddc>
                         facebook
                     </span></a></li>
-            <li class="footer-social__item"><a href="https://blog.ecosia.org" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-blog icon--size-m" data-v-16fce65c data-v-16fce65c>
+            <li class="footer-social__item"><a href="" target="_blank" rel="noreferrer" data-track-id="footer" data-test-id="footer-social__link" class="footer-social__link link link--as-a link--color-secondary" data-v-b4e44ddc><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon icon--icon-blog icon--size-m" data-v-16fce65c data-v-16fce65c>
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M1 12.015C1 5.932 5.858 1 11.85 1 17.843 1 22.7 5.932 22.7 12.015c0 6.084-4.857 11.015-10.85 11.015C5.858 23.03 1 18.1 1 12.015zm6.637-.113c.445-.159.71-.65.71-1.125 0-.839-.65-1.368-1.768-1.368H4.147v5.136H6.73c1.156 0 1.843-.573 1.843-1.427 0-.551-.31-1.08-.936-1.216zm1.548-2.72v5.364h1.231V9.183h-1.23zm3.89 1.602c-1.163 0-2.016.823-2.016 1.926 0 1.118.853 1.934 2.016 1.934 1.156 0 2.01-.816 2.01-1.934 0-1.103-.854-1.926-2.01-1.926zm5.265.098v.181a1.81 1.81 0 00-1.005-.279c-.99 0-1.82.748-1.82 1.866 0 .99.83 1.767 1.82 1.767.408 0 .733-.098 1.005-.264v.438c0 .498-.325.755-.74.755-.454 0-.71-.211-.77-.551h-1.21c.061.951.87 1.586 1.98 1.586 1.148 0 1.963-.665 1.963-1.813v-3.686H18.34zm-11.957-.43c.49 0 .748.173.748.498 0 .333-.265.574-.748.574h-.96v-1.072h.96zm.181 3.036h-1.14v-1.035h1.163c.476 0 .74.182.74.499 0 .302-.234.536-.763.536zm6.511.136c-.521 0-.868-.385-.868-.906 0-.506.347-.907.868-.907.514 0 .861.4.861.907 0 .52-.347.906-.86.906zm4.51-.181c-.499 0-.87-.393-.87-.793 0-.53.371-.861.87-.861.264 0 .52.075.755.34v.966a.954.954 0 01-.756.348z" fill="currentColor" data-v-16fce65c data-v-16fce65c></path>
                     </svg> <span class="sr-only" data-v-b4e44ddc>
                         blog
                     </span></a></li>
         </ul>
-        <p class="footer-social__legal">Ecosia GmbH does not assume responsibility for the content of
-            sites to which it links and the way in which search results are displayed. To learn more
-            please read our <a href="https://info.ecosia.org/privacy" target="_blank" rel="noopener">privacy policy</a> and our <a href="https://explore.ecosia.org/ecosia-search-tos" target="_blank" rel="noopener">terms
-                of service</a>.</p>
+        <p class="footer-social__legal">Developed by Team Morse Coders.</p>
     </div>
 </footer>
