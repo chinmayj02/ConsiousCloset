14043f9d11f4d53cd38561de3e074a86871058c3
VadirajInamdar <vadirajinamdar6@gmail.com>
2023-03-23 23:55:28 +0530

    Database

--- index.php
+++ index.php
@@ -44,57 +44,21 @@
             </div>
         </section>
         <!-- store buttons -->
-        <section class="section-wrapper extension-section section-wrapper--default" id="store_section">
-            <div class="section-wrapper__content">
-                <div class="extension-section__slot">
-                <div class="extension-section__slides flex-side">
-                    <div class="card1" style="background-color:#fff"></div>
-                    <div class="card2"></div>
+        <div class="store-section">
+            <div class="Cards">
+                
+                <div class="card1">   
+                    <button id="card-1" class="btn btn-success"><span class="b1text">Store</span></button>
                 </div>
-                    <div class="extension-section__slides">
-                        <div>
-                            <!-- <img class="thrift-image" src="images/markus-winkler-PQmXUxmfR44-unsplash.jpg"></div> -->
-                        </div>
-                        <div class="extension-section__text">
-                            <div class="section-header extension-section__header">
-
-                                <h3 class="section-header__subtitle">
-                                    Make the choice for a better future
-                                </h3>
-                            </div>
-                            <div class="extension-section__contents" style="width:300%;transform:translateX(0%);">
-                                <div class="extension-section__content">
-                                    <div class="extension-section__title">
-                                        Add the extension for quick searches
-                                    </div>
-                                    <p class="extension-section__description">
-                                        No need to visit the Ecosia website. Plant trees with searches typed
-                                        directly into your address bar or the search bar shown on a new tab.
-                                    </p>
-                                </div>
-                                <div class="extension-section__content">
-                                    <div class="extension-section__title">
-                                        Watch your impact grow
-                                    </div>
-                                    <p class="extension-section__description">
-                                        Track your tree-planting progress and explore your impact on ecosystems and
-                                        communities.
-                                    </p>
-                                </div>
-                                <div class="extension-section__content">
-                                    <div class="extension-section__title">
-                                        Make greener choices faster
-                                    </div>
-                                    <p class="extension-section__description">
-                                        See which companies lead to a greener world.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                
+                <div class="card2">
+                    <button id="card-2" class="btn btn-success"><span class="b2text">Thrifting</span></button>
                 </div>
-        </section>
-
+            </div>
+            <div class="Text">
+                    <h3 id="store-sec-text">" Make the choice for a better future "</h3>
+            </div>
+        </div>
         <!-- our initiative -->
         <section class="section-wrapper projects-section section-wrapper--dark-green" id="our_initiative">
             <div class="section-wrapper__content">
@@ -154,7 +118,7 @@
                 </ul>
             </div>
         </section>
-        <section class="section-wrapper financial-report-section section-wrapper--gray" id="aboutus">
+        <!-- <section class="section-wrapper financial-report-section section-wrapper--gray" id="aboutus">
             <div class="section-wrapper__content" style="flex-direction:column;justify-content:space-around;">
                 <div class="financial-report-section__slot">
                     <div class="financial-report-section__content">
@@ -230,8 +194,8 @@
             <div class="section-wrapper__content">
                 <div class="join-section--content">
                     <h2 data-test-id="join-section-heading" class="join-section__heading" style="font-size: 35px;">
-                    A commitment to sustainability shouldn't just be buzzwords and vague slogans.<br>
-                    Let us join hands and shape our future
+                        A commitment to sustainability shouldn't just be buzzwords and vague slogans.<br>
+                        Let us join hands and shape our future
                     </h2>
                     <div class="join-section--counter-wrapper">
                         <div data-test-id="join-section-counter" class="join-section__counter tree-counter-section">
@@ -239,12 +203,101 @@
                                     <path d="M37.687 16.915c-.518-.7-1.415-.914-2.207-.757-1.581.315-3.192 1.358-3.877 1.843a.04.04 0 01-.03.01.041.041 0 01-.028-.014.043.043 0 01.003-.06c1.093-1.216 4.648-5.117 3.69-7.23-.385-.85-1.428-1.042-2.27-.82-1.402.37-3.336 1.597-3.82 1.912a.04.04 0 01-.049-.004.043.043 0 01-.011-.049c.217-.528 1.03-2.643 1.203-5.03.247-3.435-.396-7.074-4.303-6.688-6.75.668-2.89 10.694-2.447 11.792a.043.043 0 01-.008.048.04.04 0 01-.022.012.04.04 0 01-.024-.004c-.425-.187-2.21-.773-3.632-.966-2.058-.282-2.987 1.246-1.683 2.921a63.903 63.903 0 003.569 4.19c.006.006.01.014.012.023 0 .01-.001.019-.006.027a.042.042 0 01-.02.017.04.04 0 01-.026.002c-.439-.112-1.842-.54-3.448-.56-1.386-.017-2.18.686-2.244 1.754-.15 2.545 3.255 5.09 8.713 10.05a.042.042 0 01.013.026c.15 1.144.6 6.228-1.171 17.669a.808.808 0 00.174.635.772.772 0 00.585.277h5.303a.752.752 0 00.584-.277.795.795 0 00.174-.635c-1.771-11.442-1.32-16.526-1.171-17.67a.042.042 0 01.014-.026c.402-.353 10.665-9.433 8.46-12.418z" fill="var(--color-tree-counter-tree-tall)"></path>
                                     <path d="M13.842 28.44a.029.029 0 01-.03-.029.029.029 0 01.03-.03c.822-.15 3.87-.933 3.048-4.201-.895-3.557-4.08-2.345-4.82-2.01a.029.029 0 01-.019.002.029.029 0 01-.014-.047c.377-.398 1.455-1.748.684-3.467-.5-1.117-1.719-2.872-4.487-2.057-4.75 1.4-2.496 5.032-1.988 5.757a.029.029 0 01-.02.044.029.029 0 01-.019-.003c-.714-.447-3.59-1.646-4.682 1.215-.85 2.225.97 3.43 1.297 3.647a.029.029 0 01-.012.052c-.402.062-2.043.728-2.3 2.189-.142.802.135 1.844 1.264 3.158 1.568 1.825 5.356.143 5.83-.155a.03.03 0 01.04.008c.002.005.004.01.004.015.073 1.718.308 9.233-.906 14.702-.08.358.16.652.53.652h3.322c.37 0 .61-.293.53-.652-1.145-5.162-1-12.148-.92-14.36 0-.006.003-.011.006-.016a.03.03 0 01.043-.006c.785.694 4.478 3.683 6.518.577 2.589-3.94-1.906-4.836-2.929-4.986z" fill="var(--color-tree-counter-tree-short)"></path>
                                 </svg> </div>
-                            
+
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
+        <section class="section-wrapper financial-report-section section-wrapper--gray" id="aboutus">
+            <div class="section-wrapper__content" style="flex-direction:column;justify-content:space-around;">
+                <div class="financial-report-section__slot">
+                    <div class="financial-report-section__content">
+                        <div data-test-id="financial-report-section-header" class="section-header financial-report-section__header">
+                            <h2 class="section-header__title">
+                                Some statistics to provide perspective
+                            </h2>
+                            <h3 class="section-header__subtitle">
+        
+                            </h3>
+                        </div>
+                        <div class="financial-report-section__text">
+                            Producing 1 kg of cotton emits more than 7 kgs of carbon dioxide
+                        </div>
+                        <div class="financial-report-section__report">
+                            <div class="financial-report-section__total-wrapper">
+                                <div class="financial-report-section__total">
+                                    <div class="financial-report-section__total-amount">
+                                        equivalent to driving for 11.3 kilometers
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="financial-report-section__slot">
+                    <div class="financial-report-section__content">
+                        <div data-test-id="financial-report-section-header" class="section-header financial-report-section__header">
+                            <h2 class="section-header__title">
+                                Some statistics to provide perspective
+                            </h2>
+                            <h3 class="section-header__subtitle">
+        
+                            </h3>
+                        </div>
+                        <div class="financial-report-section__text">
+                            Over 2,650 liters of water is used to manufacture ONE cotton T-shirt.
+                        </div>
+                        <div class="financial-report-section__report">
+                            <div class="financial-report-section__total-wrapper">
+                                <div class="financial-report-section__total">
+                                    <div class="financial-report-section__total-amount">
+                                        enough to fill 22 bathtubs
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="financial-report-section__slot">
+                    <div class="financial-report-section__content">
+                        <div data-test-id="financial-report-section-header" class="section-header financial-report-section__header">
+                            <h2 class="section-header__title">
+                                Some statistics to provide perspective
+                            </h2>
+                            <h3 class="section-header__subtitle">
+        
+                            </h3>
+                        </div>
+                        <div class="financial-report-section__text">
+                            According to the World Bank, 17-20% of industrial water pollution is due to textile dyeing and treatment.
+                        </div>
+                        <div class="financial-report-section__report">
+                            <div class="financial-report-section__total-wrapper">
+                                <div class="financial-report-section__total">
+                                    <div class="financial-report-section__total-amount">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="financial-report-section__slot">
+                    <div class="financial-report-section__content">
+                        <div data-test-id="financial-report-section-header" class="section-header financial-report-section__header">
+                            <h2 class="section-header__title">
+                                A commitment to sustainability shouldn't just be buzzwords and vague slogans.
+                                Let us join hands and shape our future
+                            </h2>
+                            <h3 class="section-header__subtitle">
+        
+                            </h3>
+                        </div>
+        
 
         <?php require "controllers/includes/footer.php" ?>
     </div>
