70032fdd73f651e5868c6ef8ea3ff094d1925857
Chinmay Joshi <chinmayujoshi@gmail.com>
2023-03-23 17:48:23 +0530

    inline logo size

--- controllers/includes/header.php
+++ controllers/includes/header.php
@@ -2,7 +2,7 @@
     <nav class="navbar fixed-top navbar-expand-lg">
         <div class="container-fluid">
             <div class="brand">
-                <a class="navbar-brand" href="#"><img src="./images/logo.png" alt="Logo">
+                <a class="navbar-brand" href="#"><img src="./images/logo.png" alt="Logo" style="width: 60px;">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
